6405e2d064fbe9fecba8a63f15e4afb0142bf408
unknown <Administrador@L1-LCC0409-17.grupoutp.net>
2026-04-15 09:40:40 -0500

    v1 iterativa

---
+++ Factorial.java
@@ -0,0 +1,8 @@
+// v1: iterativa
+public class Factorial {
+    public static long calcular(int n) {
+        long res = 1;
+        for (int i = 1; i <= n; i++) res *= i;
+        return res;
+    }
+}
